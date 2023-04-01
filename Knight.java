ccdd10cc8f6dda3186a3d6f13f4a5a5c89293ab4
Joshua Lester <127695056+Joshua-Lester3@users.noreply.github.com>
2023-03-31 21:23:14 -0700

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,6 +1,6 @@
 package Chess.Game;
 
-import java.awt.*;
+import java.awt.Point;
 
 public class Knight extends Piece {
     protected Knight(Point point, boolean isWhite) {
@@ -11,26 +11,31 @@ public class Knight extends Piece {
         super(x, y, 'k', isWhite);
     }
 
-    // Checks the eight available moves given by knights. We can assume the point entered is
-    // valid because Driver makes sure it is
+    // Checks the eight available moves given by knights
     @Override
-    protected boolean isLegalMove(Point targetPos, boolean isSpaceOccupied) {
-        boolean topLeft1 = super.getX() - 2 == targetPos.x && super.getY() - 1 == targetPos.y;
-        boolean topLeft2 = super.getX() - 1 == targetPos.x && super.getY() - 2 == targetPos.y;
-        boolean topRight1 = super.getX() - 1 == targetPos.x && super.getY() + 2 == targetPos.y;
-        boolean topRight2 = super.getX() - 2 == targetPos.x && super.getY() + 1 == targetPos.y;
-        boolean bottRight1 = super.getX() + 2 == targetPos.x && super.getY() + 1 == targetPos.y;
-        boolean bottRight2 = super.getX() + 1 == targetPos.x && super.getY() + 2 == targetPos.y;
-        boolean bottLeft1 = super.getX() + 2 == targetPos.x && super.getY() - 1 == targetPos.y;
-        boolean bottLeft2 = super.getX() + 1 == targetPos.x && super.getY() - 2 == targetPos.y;
-        return topLeft1 || topLeft2 || topRight1 || topRight2 || bottRight1 || bottRight2 || bottLeft1 || bottLeft2;
+    protected boolean isLegalMove(Point targetPosition, ChessBoard board) {
+        boolean isSpaceEmpty = board.get(targetPosition) instanceof EmptyPiece;
+        return isLegalMoveHelper(targetPosition) && isSpaceEmpty;
     }
 
     @Override
-    protected boolean isLegalTakingMove(Piece targetPos, boolean isSpaceOccupied) {
-        if (targetPos.getIsWhite() == super.getIsWhite()) {
+    protected boolean isLegalTakingMove(Point targetPosition, boolean targetIsWhite, ChessBoard board) {
+        if (targetIsWhite == super.getIsWhite()) {
             return false;
         }
-        return this.isLegalMove(new Point(targetPos.getX(),targetPos.getY()), isSpaceOccupied) && isSpaceOccupied;
+        boolean isSpaceEmpty = board.get(targetPosition) instanceof EmptyPiece;
+        return this.isLegalMoveHelper(targetPosition) && !isSpaceEmpty;
+    }
+
+    private boolean isLegalMoveHelper(Point targetPosition) {
+        boolean topLeft1 = super.getX() - 2 == targetPosition.x && super.getY() - 1 == targetPosition.y;
+        boolean topLeft2 = super.getX() - 1 == targetPosition.x && super.getY() - 2 == targetPosition.y;
+        boolean topRight1 = super.getX() - 1 == targetPosition.x && super.getY() + 2 == targetPosition.y;
+        boolean topRight2 = super.getX() - 2 == targetPosition.x && super.getY() + 1 == targetPosition.y;
+        boolean bottRight1 = super.getX() + 2 == targetPosition.x && super.getY() + 1 == targetPosition.y;
+        boolean bottRight2 = super.getX() + 1 == targetPosition.x && super.getY() + 2 == targetPosition.y;
+        boolean bottLeft1 = super.getX() + 2 == targetPosition.x && super.getY() - 1 == targetPosition.y;
+        boolean bottLeft2 = super.getX() + 1 == targetPosition.x && super.getY() - 2 == targetPosition.y;
+        return topLeft1 || topLeft2 || topRight1 || topRight2 || bottRight1 || bottRight2 || bottLeft1 || bottLeft2;
     }
 }
